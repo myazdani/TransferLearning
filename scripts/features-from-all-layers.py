39e2c2e600092a1af445ed0c111375d9477aab8d
Mehrdad Yazdani <myazdani@gmail.com>
2016-01-20 14:31:55 -0800

    modified to only write last output layer

--- scripts/features-from-all-layers.py
+++ scripts/features-from-all-layers.py
@@ -5,8 +5,8 @@ from skicaffe import SkiCaffe
 
 caffe_root = '/usr/local/src/caffe/caffe-master/'
 DLmodel = SkiCaffe('/usr/local/src/caffe/caffe-master/')
-model_prototxt = caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt'
-model_trained = caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+model_prototxt = caffe_root + 'models/bvlc_googlenet/deploy.prototxt'
+model_trained = caffe_root + 'models/bvlc_googlenet/bvlc_googlenet.caffemodel'
 
 DLmodel.fit(model_prototxt_path = model_prototxt, model_trained_path = model_trained)
 
@@ -17,7 +17,7 @@ for root, dirs, files in os.walk(src_path):
     image_paths.extend([os.path.join(root, f) for f in files if f.endswith(image_type)])
 
 for key in DLmodel.layer_dict.keys():
-  if key == 'prob' or key == 'data':
+  if key != 'pool5/7x7_s1':
     continue
   image_features = DLmodel.transform(image_paths = image_paths, layer_name = key, return_type = 'pandasDF')
-  image_features.to_csv("../features/BVLCref_" + key + ".csv")
+  image_features.to_csv("../features/GoogLeNet_out.csv")
